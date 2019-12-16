ecf3bf6517ac63a60fb39460f89611b93a0850f5
Roman <38250358+leprom@users.noreply.github.com>
2019-12-16 07:00:52 +0200

    Update Prediction.java

--- src/Prediction.java
+++ src/Prediction.java
@@ -1,6 +1,18 @@
 public class Prediction {
 
+ FactSheet factSheet;
+    Weather weather;
 
-    
+    /**
+     * Das vorhergesagte Wetter wird zufällig gewählt
+     * @param factSheet
+     */
+    public Prediction(FactSheet factSheet) {
+        this.factSheet = factSheet;
+
+        double random = Math.random();
+        if(random>=0.5) weather=Weather.BAD;
+        else weather=Weather.GOOD;
+    }
 
 }
